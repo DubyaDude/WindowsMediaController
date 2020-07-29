d6f97c5762c17ec5467cd44f28fee1adc538a5ce
Usman Shafiq <xxxshadowcraftxxx@gmail.com>
2020-07-28 20:02:22 -0400

    Initial Commit

---
+++ Test/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsMediaController;
+using Windows.Media.Control;
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MediaManager.OnNewSource += MediaManager_OnNewSource;
+            MediaManager.OnRemovedSource += MediaManager_OnRemovedSource;
+            MediaManager.OnPlaybackStateChanged += MediaManager_OnPlaybackStateChanged;
+            MediaManager.OnSongChanged += MediaManager_OnSongChanged;
+            MediaManager.Start();
+            while (true)
+                Console.ReadLine();
+        }
+        private static void MediaManager_OnNewSource(MediaManager.MediaSession session)
+        {
+            WriteLineColor("-- New Source: " + session.ControlSession.SourceAppUserModelId, ConsoleColor.Green);
+        }
+        private static void MediaManager_OnRemovedSource(MediaManager.MediaSession session)
+        {
+            WriteLineColor("-- Removed Source: " + session.ControlSession.SourceAppUserModelId, ConsoleColor.Red);
+        }
+
+        private static void MediaManager_OnSongChanged(MediaManager.MediaSession sender, GlobalSystemMediaTransportControlsSessionMediaProperties args)
+        {
+            WriteLineColor($"{sender.ControlSession.SourceAppUserModelId} is now playing {args.Title} {(String.IsNullOrEmpty(args.Artist) ? "" : $"by {args.Artist}")}", ConsoleColor.Cyan);
+        }
+
+        private static void MediaManager_OnPlaybackStateChanged(MediaManager.MediaSession sender, GlobalSystemMediaTransportControlsSessionPlaybackInfo args)
+        {
+            WriteLineColor($"{sender.ControlSession.SourceAppUserModelId} is now {args.PlaybackStatus}", ConsoleColor.Magenta);
+        }
+
+
+        public static void WriteLineColor(object toprint, ConsoleColor color = ConsoleColor.Gray)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine("[" + DateTime.Now.ToString("HH:mm:ss.fff") + "] " + toprint);
+        }
+    }
+}
---
+++ WindowsMediaController/Main.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Media.Control;
+
+namespace WindowsMediaController
+{
+    public static class MediaManager
+    {
+        public delegate void MediaSessionDelegate(MediaSession session);
+
+        /// <summary>
+        /// Triggered when a new media source gets added to the MediaSessions
+        /// </summary>
+        public static event MediaSessionDelegate OnNewSource;
+
+        /// <summary>
+        /// Triggered when a media source gets removed from the MediaSessions
+        /// </summary>
+        public static event MediaSessionDelegate OnRemovedSource;
+
+        /// <summary>
+        /// Triggered when a playback state changes of a MediaSession
+        /// </summary>
+        public static event TypedEventHandler<MediaSession, GlobalSystemMediaTransportControlsSessionPlaybackInfo> OnPlaybackStateChanged;
+
+        /// <summary>
+        /// Triggered when a song changes of a MediaSession
+        /// </summary>
+        public static event TypedEventHandler<MediaSession, GlobalSystemMediaTransportControlsSessionMediaProperties> OnSongChanged;
+        
+        /// <summary>
+        /// A dictionary of the current MediaSessions
+        /// </summary>
+        public static Dictionary<string, MediaSession> CurrentMediaSessions = new Dictionary<string, MediaSession>();
+
+
+        private static bool IsStarted;
+
+        /// <summary>
+        /// This starts the MediaManager
+        /// This can be changed to a constructor if you don't care for the first few 'new sources' events
+        /// </summary>
+        public static void Start()
+        {
+            if (!IsStarted)
+            {
+                var sessionManager = GlobalSystemMediaTransportControlsSessionManager.RequestAsync().GetAwaiter().GetResult();
+                SessionsChanged(sessionManager);
+                sessionManager.SessionsChanged += SessionsChanged;
+                IsStarted = true;
+            }
+        }
+
+        private static void SessionsChanged(GlobalSystemMediaTransportControlsSessionManager sender, SessionsChangedEventArgs args = null)
+        {
+            var sessionList = sender.GetSessions();
+
+            foreach (var session in sessionList)
+            {
+                if (!CurrentMediaSessions.ContainsKey(session.SourceAppUserModelId))
+                {
+                    MediaSession mediaSession = new MediaSession(session);
+                    CurrentMediaSessions[session.SourceAppUserModelId] = mediaSession;
+                    OnNewSource?.Invoke(mediaSession);
+                    mediaSession.OnSongChange(session);
+                }
+            }
+        }
+
+
+        private static void RemoveSession(MediaSession mediaSession)
+        {
+            CurrentMediaSessions.Remove(mediaSession.ControlSession.SourceAppUserModelId); 
+            OnRemovedSource?.Invoke(mediaSession);
+        }
+
+        public class MediaSession
+        {
+            public GlobalSystemMediaTransportControlsSession ControlSession;
+            public string LastSong;
+
+            public MediaSession(GlobalSystemMediaTransportControlsSession ctrlSession)
+            {
+                ControlSession = ctrlSession;
+                ControlSession.MediaPropertiesChanged += OnSongChange;
+                ControlSession.PlaybackInfoChanged += OnPlaybackInfoChanged;
+            }
+
+
+            private void OnPlaybackInfoChanged(GlobalSystemMediaTransportControlsSession session, PlaybackInfoChangedEventArgs args = null)
+            {
+                var props = session.GetPlaybackInfo();
+                if (props.PlaybackStatus == GlobalSystemMediaTransportControlsSessionPlaybackStatus.Closed)
+                {
+                    session.PlaybackInfoChanged -= OnPlaybackInfoChanged;
+                    session.MediaPropertiesChanged -= OnSongChange;
+                    RemoveSession(this);
+                }
+                else
+                {
+                    OnPlaybackStateChanged?.Invoke(this, props);
+                }
+            }
+
+
+            internal async void OnSongChange(GlobalSystemMediaTransportControlsSession session, MediaPropertiesChangedEventArgs args = null)
+            {
+                var props = await session.TryGetMediaPropertiesAsync();
+                string song = $"{props.Title} | {props.Artist}";
+
+                //This is needed because for some reason this method is invoked twice every song change
+                if (LastSong != song && !(String.IsNullOrWhiteSpace(props.Title) && String.IsNullOrWhiteSpace(props.Artist)))
+                {
+                    LastSong = song;
+                    OnSongChanged?.Invoke(this, props);
+                }
+            }
+        }
+
+    }
+}
